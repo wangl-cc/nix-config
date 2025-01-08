22126edc9021fb4f28a2c8dd27fb8948d9912859
Loong <wangl.cc@outlook.com>
2025-01-08 20:46:21 +0000

    Added profile dir for share config between hosts and users

---
+++ profiles/normal.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    dust
+  ];
+  imports =
+    builtins.map (path: ../packages/${path}) [
+      "atuin"
+      "bat"
+      "delta"
+      "eza"
+      "fastfetch"
+      "fd"
+      "fish"
+      "fzf"
+      "gh"
+      "git"
+      "gnupg"
+      "lazygit"
+      "neovim"
+      "pinentry-mac"
+      "ripgrep"
+      "starship"
+      "zoxide"
+    ]
+    ++ builtins.map (path: ../develop/${path}) [
+      "julia"
+      "latex"
+      "nix"
+      "python"
+      "rust"
+      "zed"
+    ];
+}
